49250e843e07126b0cd33be35a72d37d6fa6fb7b
Mike Howe <mike@mcsnetworks.com>
2015-07-15 18:04:12 -0500

    Add mob.
    Signed-off-by: Mike Howe <mike@mcsnetworks.com>

--- src/com/almuramc/resprotect/EntitySpawnListener.java
+++ src/com/almuramc/resprotect/EntitySpawnListener.java
@@ -154,7 +154,7 @@ public class EntitySpawnListener implements Listener {
             
             // Thermal Foundation
             if (config.getBoolean("EnableTFHooks")) {
-                if (entityName.equalsIgnoreCase("ThermalFoundation-Blitz") || entityName.equalsIgnoreCase("ThermalFoundation-Basalz")) {
+                if (entityName.equalsIgnoreCase("ThermalFoundation-Blitz") || entityName.equalsIgnoreCase("ThermalFoundation-Basalz") || entityName.equalsIgnoreCase("ThermalFoundation-Blizz")) {
                     found = true;
                     if(!perms.has("tf-monsters", true)) {
                         event.setCancelled(true);
